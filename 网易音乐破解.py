c2227bf7c18240782c8fc12caf55b0183599d9e3
gftv <30892975+gftv@users.noreply.github.com>
2018-01-21 18:00:51 +0800

    Update 网易音乐破解.py

--- 网易音乐破解.py
+++ 网易音乐破解.py
@@ -27,7 +27,7 @@ while 1:
                 print(len(k))
                 ta=k
             for i in ta:
-                if i.split('.')=='uc':
+                if i.split('.')[-1]=='uc':
                     crash(i)
         print('all finish')
     except Exception as err:
