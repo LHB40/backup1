68eeb6a3c65a66f47e0853a45278a2d633ea6de2
gftv <30892975+gftv@users.noreply.github.com>
2020-08-08 21:33:27 +0800

    Delete Python 隐藏console.py

--- Python 隐藏console.py
+++
@@ -1,5 +0,0 @@
-import ctypes    
-whnd = ctypes.windll.kernel32.GetConsoleWindow()    
-if whnd != 0:    
-    ctypes.windll.user32.ShowWindow(whnd, 0)    
-    ctypes.windll.kernel32.CloseHandle(whnd)   
